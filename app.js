405e4c0eb3acab097115e2172fe55f9acdedcda2
Hudson <Simon.Hudson@experian.com>
2020-05-20 10:54:42 +0100

    return val

--- app.js
+++ app.js
@@ -7,14 +7,14 @@ const PORT = 3000;
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-const fetchData = async () => {
+const fetchData = async (res) => {
   const result = await axios.get('https://www.moneysavingexpert.com/latesttip#martinappearances');
   const dom = cheerio.load(result.data);
   const element = dom('#martinappearances');
   res.send(element.text());
 };
 
-app.get('/', (req, res) => fetchData());
-app.post('/', (req, res) => fetchData());
+app.get('/', (req, res) => fetchData(res));
+app.post('/', (req, res) => fetchData(res));
 
 app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT}`));
