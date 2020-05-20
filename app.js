6ede7b94ee28edea75a599513115af08a8641714
Hudson <Simon.Hudson@experian.com>
2020-05-20 11:07:58 +0100

    app start up

--- app.js
+++ app.js
@@ -1,20 +1,39 @@
 'use strict';
 
 require('dotenv-safe').config();
-const express = require('express');
-const app = express();
 const PORT = 3000;
+const dev = process.env.NODE_ENV !== 'production';
+
+const express = require('express');
+const next = require('next');
+const app = next({ dev });
 const axios = require('axios');
+const bodyParser = require('body-parser');
 const cheerio = require('cheerio');
+const handle = app.getRequestHandler();
 
 const fetchData = async (res) => {
-  const result = await axios.get('https://www.moneysavingexpert.com/latesttip#martinappearances');
+  const result = await axios.get(process.env.SCRAPE_URL);
   const dom = cheerio.load(result.data);
   const element = dom('#martinappearances');
   res.send(element.text());
 };
 
-app.get('/', (req, res) => fetchData(res));
-app.post('/', (req, res) => fetchData(res));
+app.prepare()
+	.then(() => {
+		const server = express();
+		server.use(bodyParser.urlencoded({ extended: true }));
+		server.use(bodyParser.json());
 
-// app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT}`));
+		server.get('/', (req, res) => fetchData(res));
+    server.post('/', (req, res) => fetchData(res));
+
+		server.listen(3000, (err) => {
+			if (err) throw err;
+			console.log('App ready on http://localhost:3000');
+		});
+	})
+	.catch((ex) => {
+		console.error(ex.stack);
+		process.exit(1);
+	});
